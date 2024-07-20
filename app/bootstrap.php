ba9a7fb86a50e626736994b47bf2943c8a53773f
yoga.agung <yoga.agung@transtrack.id>
2024-07-20 11:23:47 +0700

    move routes

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -9,17 +9,14 @@ if (!function_exists('dd')) {
      */
     function dd(...$vars): void
     {
-        // Start output buffering
         ob_start();
         
-        // Display variables
         echo "<pre style='background: #f8f9fa; padding: 1em; border: 1px solid #dee2e6; border-radius: .25rem;'>";
         foreach ($vars as $var) {
             var_export($var);
             echo "\n";
         }
         
-        // Display any captured error
         $error = error_get_last();
         if ($error) {
             echo "\n[ERROR]: ";
@@ -28,7 +25,6 @@ if (!function_exists('dd')) {
 
         echo "</pre>";
 
-        // Flush output buffer and stop execution
         ob_end_flush();
         die();
     }
--- public/index.php
+++ public/index.php
@@ -1,34 +1,8 @@
 <?php
 
 use Yogaap\PHP\MVC\App\Router;
-use Yogaap\PHP\MVC\Http\Controllers\HomeController;
-use Yogaap\PHP\MVC\Http\Controllers\UserController;
-use Yogaap\PHP\MVC\Middleware\AuthMiddleware;
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
-Router::add('GET', '/', HomeController::class, 'index');
-
-Router::group(['middlewares' => [AuthMiddleware::class]], function () {
-    Router::group(['prefix' => 'users', 'controller' => UserController::class], function () {
-        Router::add('GET', '/login', 'loginPage');
-        Router::add('POST', '/login', 'login');
-
-        Router::add('GET', '/register', 'registerPage');
-        Router::add('POST', '/register', 'register');
-    
-        Router::add('GET', '/profile', 'profilePage');
-        Router::add('POST', '/profile/{id}', 'profile');
-
-        Router::add('GET', '/password', 'passwordPage');
-        Router::add('POST', '/password/{id}', 'password');
-
-        Router::add('GET', '/logout', 'logout');
-    });
-
-    Router::group(['prefix' => 'home', 'controller' => HomeController::class], function () {
-        Router::add('GET', '/dashboard', 'dashboard');
-    });
-});
+require_once __DIR__ . '/../routes/web.php';
 
 Router::run();
